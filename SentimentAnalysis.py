18d6ac0286e2da4a4c3d52321ccf0466790d3c8f
Rahul Gupta <rahul@growingdata.com.au>
2018-05-16 18:19:31 +1000

    Sentiment Analysis Graph

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -5,6 +5,7 @@
 import string
 import re
 import tweepy
+import json
 import twitterClient
 
 import nltk
@@ -17,10 +18,32 @@ from colorama import Fore, Style
 import pandas as pd
 import matplotlib.pyplot as plt
 
+class TwitterProcessing:
+    def __init__(self, tokeniser, lStopwords):
+        self.tokeniser = tokeniser
+        self.lStopwords = lStopwords
+
+    def process(self, text):
+        text = text.lower()
+        tokens = self.tokeniser.tokenize(text)
+        tokensStripped = [tok.strip() for tok in tokens]
+        regexDigit = re.compile("^\d+\s|\s\d+\s|\s\d+$")
+        regexHttp = re.compile("^http")
+
+        return [tok for tok in tokensStripped if tok not in self.lStopwords and
+                regexDigit.match(tok) == None and regexHttp.match(tok) == None]
+
+
+def computeSentiment(lTokens, setPosWords, setNegWords):
+    posNum = len([tok for tok in lTokens if tok in setPosWords])
+    negNum = len([tok for tok in lTokens if tok in setNegWords])
+
+    sentimentVal = posNum - negNum
+    return sentimentVal
+
 
 def countWordSentimentAnalysis(setPosWords, setNegWords, tweets, tweetProcessor):
     lSentiment = []
-
     for tweet in tweets:
         try:
             tweetText = tweet['text']
@@ -29,7 +52,6 @@ def countWordSentimentAnalysis(setPosWords, setNegWords, tweets, tweetProcessor)
             lTokens = tweetProcessor.process(tweetText)
             sentiment = computeSentiment(lTokens, setPosWords, setNegWords)
             lSentiment.append([pd.to_datetime(tweetDate), tweetText, sentiment])
-
         except KeyError as e:
             pass
 
@@ -63,57 +85,26 @@ def vaderSentimentAnalysis(tweets, bPrint, tweetProcessor):
 
     return lSentiment
 
-
-def computeSentiment(lTokens, setPosWords, setNegWords):
-    pos_words = 0
-    neg_words = 0
-
-    if lTokens in setPosWords:
-        pos_words += 1
-    elif lTokens in setNegWords:
-        neg_words += 1
-    else:
-        pass
-
-    if pos_words > neg_words:
-        return ('Positive')
-    elif pos_words < neg_words:
-        return ('Negative')
-    elif pos_words == neg_words:
-        return ('Neutral')
-
-
-class TwitterProcessing:
-
-    def __init__(self, tokeniser, lStopwords):
-        self.tokeniser = tokeniser
-        self.lStopwords = lStopwords
-
-    def process(self, text):
-        text = text.lower()
-        tokens = self.tokeniser.tokenize(text)
-        tokensStripped = [tok.strip() for tok in tokens]
-        regexDigit = re.compile("^\d+\s|\s\d+\s|\s\d+$")
-        regexHttp = re.compile("^http")
-
-        return [tok for tok in tokensStripped if tok not in self.lStopwords and
-                regexDigit.match(tok) == None and regexHttp.match(tok) == None]
-
-
-def remove_duplicates(l):
-    return list(set(l))
-
-
 def main():
     api = twitterClient.twitter_client()
 
     tweets = []
     query = 'streaming service OR streaming movies OR streaming series'
-    max_tweets = 3000
+    # query = 'happy'
+    max_tweets = 500
 
     # append all tweet data to list
+    file_name = 'sentiment_rawdata.json'
+
     for tweet in tweepy.Cursor(api.search, q=query, lang="en").items(max_tweets):
         tweets.append(tweet._json)
+
+    with open(file_name, 'w') as f:
+        json.dump(tweets, f)
+
+    with open(file_name, 'r') as f:
+        for line in f:
+            tweets = json.loads(line)
 
     tweetTokenizer = TweetTokenizer()
     lPunct = list(string.punctuation)
@@ -121,27 +112,29 @@ def main():
 
     tweetProcessor = TwitterProcessing(tweetTokenizer, lStopwords)
 
-    setPosWords = opinion_lexicon.positive()
-    setNegWords = opinion_lexicon.negative()
+    PosWords = opinion_lexicon.positive()
+    NegWords = opinion_lexicon.negative()
 
-    lSentiment = []
-    lSentiment = countWordSentimentAnalysis(setPosWords, setNegWords, tweets, tweetProcessor)
+    lSentiment = countWordSentimentAnalysis(PosWords, NegWords, tweets, tweetProcessor)
+    print(lSentiment)
+    print(len(lSentiment))
 
     df = pd.DataFrame(lSentiment)
-    df.columns = ['Timestamp', 'text', 'Sentiment']
-    print(df)
+    df.columns = ['Timestamp', 'text','Sentiment']
 
-    # Plot
     freq = df.groupby(['Sentiment']).size().reset_index(name='counts')
     freq.sort_values(['counts'], ascending=False, inplace=True)
     freq = freq.head(20)
+
     freq.plot(kind='bar', x='Sentiment', y='counts', color='#006699')
     ax = plt.gca().invert_yaxis()
     plt.xlabel('Word Count')
     plt.ylabel('Words')
     plt.title('Bar chart of word frequency')
     plt.legend().set_visible(False)
+    plt.show()
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()

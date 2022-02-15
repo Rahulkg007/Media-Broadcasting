f8cb7e08ea74f44a3dea971d249aaefd048dc4f3
Rahul K Gupta <Rahulkg007@gmail.com>
2022-02-15 20:33:08 +1100

    Update twitterClient.py

--- twitterClient.py
+++ twitterClient.py
@@ -5,10 +5,10 @@ import tweepy as tw
 def twitter_auth():
 
     try:
-        consumer_key = "q3jskpUCOy2h64TgLIJVk4lYG"
-        consumer_secret = "ihKjoH66jRlTypi5oFSye5exKnNyxpbLJREd5kf5ivrqBq9YjA"
-        access_token = "36773543-oNFUjf69C58EKhCBNoeWHUVTrHHAKnmtCCvJBeD9D"
-        access_secret = "u8milltoVvrRPAEl05TC70PbCuFiV8FH0g2t1BkUFD0a5"
+        consumer_key = "x"
+        consumer_secret = "x"
+        access_token = "x-x"
+        access_secret = "x"
     except KeyError:
         sys.stderr.write("Key or secret token are invalid.\n")
         sys.exit(1)
